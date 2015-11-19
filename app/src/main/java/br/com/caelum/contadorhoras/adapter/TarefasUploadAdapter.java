5b3042a31296ecb18b85e00d2d43ff006995a700
MathBrandino <m.v.brandino@gmail.com>
2015-11-19 11:21:10 -0200

    layout horas upload alterado

--- app/src/main/java/br/com/caelum/contadorhoras/adapter/TarefasUploadAdapter.java
+++ app/src/main/java/br/com/caelum/contadorhoras/adapter/TarefasUploadAdapter.java
@@ -1,14 +1,19 @@
 package br.com.caelum.contadorhoras.adapter;
 
+import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import br.com.caelum.contadorhoras.R;
 import br.com.caelum.contadorhoras.activity.ListaTarefasUploadActivity;
+import br.com.caelum.contadorhoras.dao.DiaDao;
 import br.com.caelum.contadorhoras.modelo.Tarefa;
 
 /**
@@ -50,9 +55,53 @@ public class TarefasUploadAdapter extends BaseAdapter {
             view = convertView;
         }
 
+        populaView(position, view);
+
+        return view;
+    }
+
+    private void populaView(int position, View view) {
         TextView descricao = (TextView) view.findViewById(R.id.descricao_tarefa_upload);
         descricao.setText(tarefas.get(position).getDesc());
 
-        return view;
+        TextView horas = (TextView) view.findViewById(R.id.quantidade_horas);
+        horas.setText(quantidadeHoras(tarefas.get(position)));
+    }
+
+    private String quantidadeHoras(Tarefa tarefa) {
+        String horasTrabalhadas;
+        Long contador = 0L;
+
+        DiaDao dao = new DiaDao(activity);
+        String dia = dao.getData(tarefa.getDataDia());
+        dao.close();
+        int horaInicial = tarefa.getHoraInicial();
+        int minutoInicial = tarefa.getMinutoInicial();
+        int horaFinal = tarefa.getHoraFinal();
+        int minutoFinal = tarefa.getMinutoFinal();
+        String tempoInicial = dia + " " + horaInicial + ":" + minutoInicial;
+        String tempoFinal = dia + " " + horaFinal + ":" + minutoFinal;
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+        Date dateFinal = null;
+        Date dateInicial = null;
+        try {
+            dateFinal = dateFormat.parse(tempoFinal);
+            dateInicial = dateFormat.parse(tempoInicial);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        long diferenca = dateFinal.getTime() - dateInicial.getTime();
+        contador += diferenca;
+
+        long hora = contador / (3600 * 1000);
+        long minutos = (contador % (3600 * 1000)) / (1000 * 60);
+
+        horasTrabalhadas = hora + ":" + minutos;
+
+        return horasTrabalhadas;
+
     }
 }
